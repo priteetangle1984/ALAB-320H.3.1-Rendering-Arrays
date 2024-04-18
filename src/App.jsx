ba5927191c0689fcd3d0ad79f524a9203ee3c275
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-04-17 23:03:54 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Learner from './components/Learner/Learner'
+
+function App() {
+
+  return (
+    <>
+      <Learner />
+    </>
+  )
+}
+
+export default App
